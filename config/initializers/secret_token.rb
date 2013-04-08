d4932a0565550605a8adafcb1005fbfe95f95b4f
vzl <schamane@i.ua>
2013-04-08 12:54:42 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tpvapp::Application.config.secret_token = 'b149f5fb67b39554f3a8190d22160a114f621c002356a82c4688ffb95d60709d18cae5275eba6e75fdf664707cc596356f5d61457deffadb2798864ebd2556ca'
